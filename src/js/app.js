a95b9ebbc0f370ef8288924b1abf67bf52cfff5e
roberto.soteres <roberto.soteres@gmail.com>
2017-10-17 19:13:48 +0200

    beginning with the project

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
-function x(){
-    
-    
-    return true;
-    
-    
+// Add the map
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.484225, lng: -3.701123},
+        zoom: 15
+    });
 }
