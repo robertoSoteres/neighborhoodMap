51bc8db7e61d14332c558b718acd3c04601c5986
roberto.soteres <roberto.soteres@gmail.com>
2017-10-23 20:17:22 +0200

    #issue 3

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ var lugares = [
 ];
 
 
-var Map;;
+var map;
 // create 1 lugar for every object in lugares
 var lugar = function(data) {    
 	var self = this;
@@ -50,7 +50,7 @@ var lugar = function(data) {
 	this.InfoWindow = new google.maps.InfoWindow({content: self.contentString});
     
     //create service places
-    var service = new google.maps.places.PlacesService(Map);
+    var service = new google.maps.places.PlacesService(map);
 
     
         
@@ -68,7 +68,7 @@ var lugar = function(data) {
     //  Markers
 	this.Marker = new google.maps.Marker({
 			position: new google.maps.LatLng(data.lat, data.long),
-			map: Map,
+			map: map,
             icon: 'images/cinemaImage.png',
 			title: data.nombre
 	});
@@ -82,7 +82,7 @@ var lugar = function(data) {
         '<div class="content">Lat: ' + self.lat + "</div>" +
         '<div class="content">Long: ' + self.long + "</div></div>";
         self.InfoWindow.setContent(self.contentString);
-		self.InfoWindow.open(Map, this);		
+		self.InfoWindow.open(map, this);		
 		self.Marker.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout(function(){ self.Marker.setAnimation(null); }, 3550);
 
@@ -93,7 +93,7 @@ var lugar = function(data) {
     //filter Markers
 	this.showMarker = ko.computed(function() {
 		if(this.visible() === true) {
-			this.Marker.setMap(Map);
+			this.Marker.setMap(map);
 		} else {
 			this.Marker.setMap(null);
 		}
@@ -129,8 +129,8 @@ function viewModel() {
 	this.searchTerm = ko.observable("");
 	this.listaLugares = ko.observableArray([]);
     
-    // lat-lang Map  + zoom
-	Map = new google.maps.Map(document.getElementById('map'), {
+    // lat-lang map  + zoom
+	map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 40.484225, lng: -3.701123},
         zoom: 12
     });
